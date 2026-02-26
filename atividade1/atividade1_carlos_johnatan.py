fd1fe873bf660e58886cbeede8ac770fff2908b8
Carlos Johnatan <carlosjohnatanssr2003@gmail.com>
2026-02-26 13:05:57 -0400

    Atividade 1 - Aula 02

--- atividade1/atividade1_carlos_johnatan.py
+++ atividade1/atividade1_carlos_johnatan.py
@@ -3,15 +3,16 @@
 
 #Lista de Alunos
 alunos = [
-  {"nome": "Brendo", "email": "brendo.matos@ciesa.br", "idade": 32, "curso": "CCP"},
-  {"nome": "Eva", "email": "eva@ciesa.br", "idade": 15, "curso": "ADS"},
-  {"nome": "Ed", "email": "ed@ciesabr", "idade": 12, "curso": "DIR"},
-  {"nome": "Joao", "email": "joao@cies.abr", "idade": 18, "curso": "ADS"},
+  {"nome": "Alexandre", "email": "jaocatao@exemplo.com", "idade": 20, "curso": "CCP"},
+  {"nome": "Daniel", "email": "dani_el13@exemplo.com", "idade": 20, "curso": "ADS"},
+  {"nome": "Julio", "email": "juliothiago2006@exemplo", "idade": 21, "curso": "DIR"},
+  {"nome": "Jhemesson", "email": "condejms1@ex.br", "idade": 15, "curso": "ADS"},
 ]
 
 #Lista de Cursos
 cursos = ["CCP", "ADS", "IA", "EGC"]
 
+#Função para validar atributos
 def validar_alunos(lista):
     validos = []
     invalidos = []
@@ -38,8 +39,27 @@ def validar_alunos(lista):
 
     return validos, invalidos
 
+#Validação de Alunos Válidos e Inválidos
+validos, invalidos = validar_alunos(alunos)
 
-v, i = validar_alunos(alunos)
+#Laço para mostrar os alunos de forma visível e organizada:
+print("\nAlunos Válidos\n")
 
-print("Válidos:", v)
-print("Inválidos:", i)
+for aluno in validos:
+    print(f"    Nome: {aluno['nome']}")
+    print(f"    Email: {aluno['email']}")
+    print(f"    Idade: {aluno['idade']}")
+    print(f"    Curso: {aluno['curso']}\n")
+
+print("Alunos Inválidos:\n")
+
+for item in invalidos:
+    aluno = item["aluno"]
+    erros = item["erros"]
+
+    print(f"    Nome: {aluno['nome']}")
+    print(f"    Email: {aluno['email']}")
+    print(f"    Idade: {aluno['idade']}")
+    print(f"    Curso: {aluno['curso']}")
+    print("    Erros:", ", ".join(erros))
+    print("")
