d56625606865df67178ca044011c59fdfa23a1d6
Carlos Johnatan <carlosjohnatanssr2003@gmail.com>
2026-02-26 12:46:10 -0400

    Add student validation function and data

--- atividade1/atividade1_carlos_johnatan.py
+++ atividade1/atividade1_carlos_johnatan.py
@@ -1,1 +1,45 @@
-print("Hello World!")
+#Atividade 01 - Aula 02
+#Carlos Johnatan Silva Souza Rodrigues
+
+#Lista de Alunos
+alunos = [
+  {"nome": "Brendo", "email": "brendo.matos@ciesa.br", "idade": 32, "curso": "CCP"},
+  {"nome": "Eva", "email": "eva@ciesa.br", "idade": 15, "curso": "ADS"},
+  {"nome": "Ed", "email": "ed@ciesabr", "idade": 12, "curso": "DIR"},
+  {"nome": "Joao", "email": "joao@cies.abr", "idade": 18, "curso": "ADS"},
+]
+
+#Lista de Cursos
+cursos = ["CCP", "ADS", "IA", "EGC"]
+
+def validar_alunos(lista):
+    validos = []
+    invalidos = []
+
+    for aluno in lista:
+        erros = []
+
+        if len(aluno["nome"]) < 3:
+            erros.append("Nome inválido")
+
+        if "@" not in aluno["email"] or "." not in aluno["email"]:
+            erros.append("Email inválido")
+
+        if aluno["idade"] < 16:
+            erros.append("Idade inválida")
+
+        if aluno["curso"] not in cursos:
+            erros.append("Curso inválido")
+
+        if erros:
+            invalidos.append({"aluno": aluno, "erros": erros})
+        else:
+            validos.append(aluno)
+
+    return validos, invalidos
+
+
+v, i = validar_alunos(alunos)
+
+print("Válidos:", v)
+print("Inválidos:", i)
